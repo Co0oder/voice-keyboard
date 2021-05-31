9015bc3e15df66ab624931f4b1da6904e35f2eb3
bpuhach <bpuhach@exadel.com>
2021-05-31 15:02:27 +0300

    file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KeyboardModule } from './features/keyboard/keyboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KeyboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
